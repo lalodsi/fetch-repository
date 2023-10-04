1c7b56f6d5440bfb84cac7245864ea48feb62339
Luis Eduardo Rodríguez Ramírez <LRODR250@ford.com>
2023-10-04 00:14:30 -0600

    Making search in dogs

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,51 @@ import LoginScreen from './containers/Login';
 import SearchMenu from './containers/Search';
 import ResultsComponent from './containers/ResultsBox';
 
+const buildGetQuery = (callback: (response: any) => void, direction: string, data: any = null) => {
+  // const data = null;
+  console.log(data);
+  
+  const xhr = new XMLHttpRequest();
+  xhr.withCredentials = true;
+  xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === this.DONE) {
+      callback(this.response);
+    }
+  });
+
+  xhr.open("GET", `https://frontend-take-home-service.fetch.com/${direction}`);
+  xhr.setRequestHeader("Authorization", "Bearer undefined");
+  xhr.send(data);
+}
+
 function App() {
-  const[searchText, setSearchText] = React.useState<string>("");
+  const [searchText, setSearchText] = React.useState<string>("");
   const [authenticated, setAuthenticated] = React.useState<boolean>(false);
-  const[results, setResults] = React.useState<string[]>([]);
+  const [results, setResults] = React.useState<string[]>([]);
 
   function getDogsBreads(authenticated: boolean) {
     if (authenticated) {
-      const data = null;
-      const xhr = new XMLHttpRequest();
-      xhr.withCredentials = true;
-      xhr.addEventListener("readystatechange", function () {
-        if (this.readyState === this.DONE) {
-          const Json: string[] = JSON.parse(this.response);
-          setResults(Json);
-        }
-      });
+      buildGetQuery((response) => {
+        const Json: string[] = JSON.parse(response);
+        setResults(Json);
+      }, 'dogs/breeds')
+    }
+  }
 
-      xhr.open("GET", "https://frontend-take-home-service.fetch.com/dogs/breeds?=");
-      xhr.setRequestHeader("Authorization", "Bearer undefined");
-      xhr.send(data);
+  function searchDogs(breed: string) {
+    if (authenticated) {
+      buildGetQuery((response) => {
+        if (response === 'Not Found') {
+          console.log("There is no dogs");
+        }
+        else{
+          console.log(response);
+        }
+        // console.log(JSON.parse(response));
+      },
+      'dogs/search',
+      {breeds: breed}
+      )
     }
   }
 
@@ -40,7 +65,7 @@ function App() {
           <SearchMenu
             searchText={searchText}
             setSearchText={setSearchText}
-            handleSearch={() => getDogsBreads(authenticated)}
+            handleSearch={() => searchDogs(searchText)}
           />
           <ResultsComponent options={results} clickHandler={() => {}} />
         </div>
